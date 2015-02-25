d836b729a9c5055f0c557025dc01e961e1a3ea45
yyamnk <y.yamnk@gmail.com>
2015-02-25 22:55:58 +0900

    mod: setup ricty

--- bin/osx/setup.sh
+++ bin/osx/setup.sh
@@ -76,7 +76,7 @@ fi
 #-------------------------------------------------------#
 # Ricty Font
 #-------------------------------------------------------#
-ls -l $HOME/Library/Fonts | grep "Ricty*.ttf" > /dev/null
+ls -l $HOME/Library/Fonts | grep "Ricty" > /dev/null
 if [ "$?" = 1 ]; then
     brew install ricty
 fi
