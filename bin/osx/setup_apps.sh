9d4eb3d72f41e3e004b29249ffb0d04805bc793e
yyamnk <y.yamnk@gmail.com>
2015-03-12 15:14:00 +0900

    add: reattach-to-user-namespace, fix yank error of vim in tmux
    tmux内のvimでクリップボード併用時にエラーが出るのを解消

--- bin/osx/setup_apps.sh
+++ bin/osx/setup_apps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 #****************** bin/setup_apps.sh *******************
 # created: 2015-Feb-06
-# Last Change: 2015-Mar-10.
+# Last Change: 2015-Mar-12.
 #------------------------------------------------------------
 # set up for apps
 #************************************************************
@@ -21,6 +21,7 @@ brew install mongodb
 brew install nkf
 brew install peco
 brew install postgresql
+brew install reattach-to-user-namespace # tmux内のvimでクリップボード併用を可能に
 brew install rmtrash # command for move trash
 brew install the_silver_searcher
 brew install tmux
