1c575dd6c9cbdc91912186e7d2d91445f59f58ea
yyamnk <y.yamnk@gmail.com>
2015-03-23 18:57:26 +0900

    add: setup_safari, download safari extentions

---
+++ bin/osx/setup_safari.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#****************** bin/osx/setup_safari.sh *******************
+# created: 2015-Mar-23
+# Last Change: 2015-Mar-23.
+#------------------------------------------------------------
+# safari extentions
+#************************************************************
+
+# vimari download to Download folder
+open https://github.com/guyht/vimari/releases/download/v1.10/vimari-1.10.safariextz -a Safari.app
+
+# Pocket download to Download folder
+open -a Safari.app "https://getpocket.com/apps/link/pocket-safari/?ep=4&s=WELCOME_PAGE"
