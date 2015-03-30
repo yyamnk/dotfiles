ef2ca971eb306887b0d9893f1f50a72c95d92646
yyamnk <y.yamnk@gmail.com>
2015-03-30 19:30:37 +0900

    update: default write

--- bin/osx/write_defaults.sh
+++ bin/osx/write_defaults.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 #****************** bin/write_defaults.sh *******************
 # created: 2015-Feb-10
-# Last Change: 2015-Mar-22.
+# Last Change: 2015-Mar-30.
 #
 # ref.
 # https://github.com/robb/.dotfiles/blob/master/osx/defaults.install
 # http://baqamore.hatenablog.com/entry/2013/08/01/222130
+# https://github.com/hjuutilainen/dotfiles/blob/master/bin/osx-user-defaults.sh
 #************************************************************
 
 echo "=================== write defaults ==================="
@@ -19,6 +20,8 @@ echo 'Expand save panel by default'
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 echo 'Expand print panel by default'
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+echo 'Sidebar icon size: Small'
+defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
 
 # dock
 echo 'fast mission control animation'
@@ -33,16 +36,24 @@ defaults write com.apple.dock magnification -bool false
 # disable dashboard
 defaults write com.apple.dashboard mcx-disabled -bool true
 
-
-# Cotroller
+# input
 echo 'add dvorak layout'
 defaults write com.apple.HIToolbox AppleEnabledInputSources -array-add '{InputSourceKind="Keyboard Layout"; "KeyboardLayout Name"=Dvorak; "KeyboardLayout ID"=16300;}'
+echo 'Disable smart quotes and dashes'
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+echo 'Correct spelling automatically'
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool true
 
+
+# Cotroller
 echo 'full Keyboard access'
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
 echo `enable tap`
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write com.apple.AppleMultitouchTrackpad DragLock -int 1
+defaults write com.apple.AppleMultitouchTrackpad Dragging -int 1
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
@@ -52,11 +63,15 @@ defaults write com.apple.screensaver askForPassword -int 1
 echo 'do not make ".DS_Store" over network'
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 
+echo 'Do not restore windows when quitting or re-opening apps'
+defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
+
 echo 'change screen shot format to pdf'
 defaults write com.apple.screencapture type pdf
 
 echo 'Safali history back by delete'
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool yes
+
 
 
 echo 'restart finder and dock'
