ada63a269079a9aff695833fa018359e702d09b9
yyamnk <y.yamnk@gmail.com>
2015-04-13 17:57:17 +0900

    mod: karabiner settigs, enable force c-m for virtual machine

--- karabiner/bin/import_includes.sh
+++ karabiner/bin/import_includes.sh
@@ -48,4 +48,6 @@ $cli set parameter.mouse_key_scroll_natural_direction 1
 /bin/echo -n .
 $cli set repeat.initial_wait 200
 /bin/echo -n .
+$cli set option.emacsmode_force_controlM_vm=1
+/bin/echo -n .
 /bin/echo
