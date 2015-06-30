607a5b34abbc15709bec032299f44fa16f81e703
yyamnk <y.yamnk@gmail.com>
2015-06-30 21:53:39 +0900

    mod: bin/setup_python, add matplotlib, ipython

--- bin/setup_python.sh
+++ bin/setup_python.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 #****************** bin/setup_python.sh *******************
 # created: 2015-Feb-12
-# Last Change: 2015-Feb-12.
+# Last Change: 2015-Jun-15.
 #************************************************************
 
 brew install python3
@@ -9,4 +9,6 @@ pip3 install --upgrade pip
 
 pip3 install numpy
 pip3 install scipy
+pip3 install matplotlib
+pip3 install ipython
 # pip3 install virtualenv virtualenvwrapper
