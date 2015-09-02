b404a268fc6933630075dc7abe8dae3cfa9f8982
yyamnk <y.yamnk@gmail.com>
2015-09-02 13:22:31 +0900

    add: pyenv

--- bin/setup_python.sh
+++ bin/setup_python.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 #****************** bin/setup_python.sh *******************
 # created: 2015-Feb-12
-# Last Change: 2015-Jun-15.
+# Last Change: 2015-Sep-02.
 #************************************************************
 
 brew install python3
+brew install pyenv
 pip3 install --upgrade pip
 
 pip3 install numpy
