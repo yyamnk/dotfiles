2b48b4d1fff980ae541d149a984a49e9040a5031
yyamnk <y.yamnk@gmail.com>
2015-09-08 23:26:52 +0900

    fix unlink_dots.sh

--- bin/unlink_dots.sh
+++ bin/unlink_dots.sh
@@ -1,17 +1,17 @@
 #! /usr/bin/env bash
 #****************** unlink_dots.sh *******************
 # created: 2014/12/01
-# Last Change:2015-Feb-03.
+# Last Change:2015-Sep-08.
 #
 # --- abst ---
 # unlink dotfiles
 #************************************************************
 
 # dotfiles dir
-DOTSDIR="${HOME}/Dropbox/dotfiles/"
-dotfiles="${DOTSDIR}.*"
+DOTSDIR="${HOME}/.dotfiles"
+DOTFILES="${DOTSDIR}/.*"
 
-for dots in ${dotfiles}
+for dots in ${DOTFILES}
 do
     case "${dots}" in
         *.DS_Store ) ;;
