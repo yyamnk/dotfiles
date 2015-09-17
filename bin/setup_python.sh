9ada039b07ba9aa2aba2b19885e708c47f8ac854
yyamnk <y.yamnk@gmail.com>
2015-09-17 20:51:41 +0900

    upd: setup_python, switch to miniconda

--- bin/setup_python.sh
+++ bin/setup_python.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env sh
 #****************** bin/setup_python.sh *******************
 # created: 2015-Feb-12
-# Last Change: 2015-Sep-11.
+# Last Change: 2015-Sep-17.
 #************************************************************
 
-brew install python3
 brew install pyenv
-pip3 install --upgrade pip
 
-pip3 install numpy
-pip3 install scipy
-pip3 install matplotlib
-pip3 install ipython
-pip3 install pandas
-pip3 install psycopg2
-pip3 install sqlalchemy
-pip3 install alembic
-pip3 install jedi
-# pip3 install virtualenv virtualenvwrapper
+# by miniconda
+pyenv install miniconda3-3.9.1
+pyenv rehash
+pyenv global miniconda3-3.9.1
+
+# get packages
+conda install numpy scipy matplotlib ipython pandas
+conda install numba cython
+conda install psycopg2 sqlalchemy
+pip install alembic
+pip install restview # rst viewer
