2b5bd6d844e351e60189f121ab114f8c03a297f8
yyamnk <y.yamnk@gmail.com>
2015-11-02 10:37:52 +0900

    upd: ipython, init

--- .ipython/profile_yyamnk/startup/00-init.py
+++ .ipython/profile_yyamnk/startup/00-init.py
@@ -2,16 +2,18 @@
 # -*- coding: utf-8 -*-
 # ****************** 00-init.py *******************
 # Created	 : 2015-Sep-02
-# Last Change: 2015-Sep-18.
+# Last Change: 2015-Oct-30.
 # -----------------------------------
 # ipython 起動時に自動実行
 # ************************************************************
 
 from imp import reload
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 
 from pandas import Series, DataFrame
 import pandas as pd
 from pprint import pprint as pp
 import sys
+import oprc
