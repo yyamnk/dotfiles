fe9681b0216c0acd7eaf6648a8a8c21bda5ccd04
yyamnk <y.yamnk@gmail.com>
2016-02-17 18:16:21 +0900

    upd: ipython startup

--- .ipython/profile_yyamnk/startup/00-init.py
+++ .ipython/profile_yyamnk/startup/00-init.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # ****************** 00-init.py *******************
 # Created	 : 2015-Sep-02
-# Last Change: 2015-Nov-28.
+# Last Change: 2016-Feb-17.
 # -----------------------------------
 # ipython 起動時に自動実行
 # ************************************************************
@@ -20,3 +20,9 @@ import pickle
 import math
 # import oprc
 from oprc import optimizers
+# figure settings
+import matplotlib as mpl
+import seaborn as sns
+sns.set_context('poster', rc={'figure.figsize': [8.0, 6.0], "lines.linewidth": 1.75})
+mpl.rcParams['legend.frameon'] = True
+mpl.rcParams['legend.shadow'] = True
