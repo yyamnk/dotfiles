0f24b1401c263d55b44001f0445c9f72212a9daa
yyamnk <y.yamnk@gmail.com>
2016-10-13 22:02:14 +0900

    add: gem iStats

--- bin/setup_gems.sh
+++ bin/setup_gems.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #****************** bin/setup_gems.sh *******************
 # created: 2015-Mar-12
-# Last Change: 2015-Mar-12.
+# Last Change: 2016-Oct-13.
 #------------------------------------------------------------
 # システムのgemに入れるもののみ.
 # Rails関連gemは各プロジェクトにインストール
@@ -16,3 +16,6 @@ gem install bundler
 gem install tmuxinator
 # tmuxのwindow, paneを設定してくれる
 # http://tactosh.com/2014/01/tmux-window-pane-tmuxinator/
+
+# https://github.com/Chris911/iStats, CPU温度表示
+gem install iStats
