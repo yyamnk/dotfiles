ebf2d7e0a9ba3024f5bc99e181f87559748591a6
yyamnk <y.yamnk@gmail.com>
2017-02-28 23:48:21 +0900

    add: bin, startup stript for lubuntu

---
+++ bin/set_myxkb.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#****************** .dotfiles/bin/set_myxkb.sh *******************
+# Created    : 2017-Feb-28
+# Last Change: 2017-Feb-28.
+#************************************************************
+setxkbmap -option ctrl:nocaps,shift:both_capslock dvorak
---
+++ bin/xcalib_mbp17.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#****************** .dotfiles/bin/xcalib_mbp17.sh *******************
+# Created    : 2017-Feb-28
+# Last Change: 2017-Feb-28.
+#************************************************************
+xcalib $HOME/.dotfiles/mbp17_late2011_cprofile.icc
