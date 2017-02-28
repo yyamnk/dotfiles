1bb3a615effc52c18d17e5a10afc431dc7695c9f
yyamnk <y.yamnk@gmail.com>
2017-02-28 17:55:35 +0900

    add: bin/load_xkbmap, .xkb

--- bin/load_xkbmap.sh
+++ bin/load_xkbmap.sh
@@ -4,6 +4,5 @@ if [ -s $HOME/.xkb/keymap/yyamnk ]
 then
     sleep 1
     # xkbcomp -I$HOME/.xkb $HOME/.xkb/keymap/mykbd $DISPLAY 2>/dev/null
-    xkbcomp $HOME/.xkb/keymap/yyamnk $DISPLAY 2>/dev/null
     xkbcomp -I$HOME/.xkb $HOME/.xkb/keymap/yyamnk $DISPLAY 2>/dev/null
 fi
