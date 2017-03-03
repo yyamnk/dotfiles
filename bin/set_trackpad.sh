0dc49e8772c8f81f101bbb81234041719bc203ed
yyamnk <y.yamnk@gmail.com>
2017-03-03 23:09:46 +0900

    upd: bin/set_trackpad, ignore while typing

--- bin/set_trackpad.sh
+++ bin/set_trackpad.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #****************** .dotfiles/bin/set_trackpad.sh *******************
 # Created    : 2017-Mar-02
-# Last Change: 2017-Mar-02.
+# Last Change: 2017-Mar-03.
 #************************************************************
 synclient PalmDetect=1
+syndaemon -k -i 0.5 -d  # ignore 1sec. deamon
