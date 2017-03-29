12a3acc8ee1ed4db009e2d1c09dbc2161ee7e59e
yyamnk <y.yamnk@gmail.com>
2017-03-30 02:31:10 +0900

    add: bin/tilda_delay

---
+++ bin/tilda_delay.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#****************** .dotfiles/bin/tilda_delay.sh *******************
+# Created    : 2017-Mar-30
+# Last Change: 2017-Mar-30.
+#------------------------------------------------------------
+# start tilda with delay for ubuntu-mate
+#************************************************************
+sleep 5s
+/usr/lib/ubuntu-mate/ubuntu-mate-tilda
