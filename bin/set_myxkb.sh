7b7e0ded904ac62733da759b86efcdcbb1788a19
yyamnk <y.yamnk@gmail.com>
2017-03-30 02:30:36 +0900

    upd: bin/set_myxkb

--- bin/set_myxkb.sh
+++ bin/set_myxkb.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 #****************** .dotfiles/bin/set_myxkb.sh *******************
 # Created    : 2017-Feb-28
-# Last Change: 2017-Feb-28.
+# Last Change: 2017-Mar-30.
 #************************************************************
-setxkbmap -option ctrl:nocaps,shift:both_capslock dvorak
+setxkbmap -option shift:both_capslock
+
+$HOME/.dotfiles/bin/ctrl2space.sh  # xmodemap and xcape
+# do not use setxkbmap after the script.
+# xcape does not work well.
+
+# replace menu to Super_R
+xmodmap -e 'keycode 135 = Super_R'  # #135 is menu key (rigt click)
+# replace ; and :
+# keycode  52 = semicolon colon semicolon colon
+xmodmap -e 'keycode 52 = colon semicolon'
+
+#-------------------------------------------------------#
+# vim-binding with left of a
+#-------------------------------------------------------#
+xmodmap -e 'clear lock'
+xmodmap -e 'keycode 66 = Mode_switch'  # #66 is capslock
+
+# expression
+# keycode XX = key shift+key mode_switch+key mode_switch+shift+key
+
+# default values
+# keycode  43 = d D d D  # qwty h
+xmodmap -e 'keycode 43 = d D Left'  # qwty h
+# keycode  44 = h H h H  # qwty j
+xmodmap -e 'keycode 44 = h H Down'
+# keycode  45 = t T t T  # qwty k
+xmodmap -e 'keycode 45 = t T Up'
+# keycode  46 = n N n N  # qwty l
+xmodmap -e 'keycode 46 = n N Right'
