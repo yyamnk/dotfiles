d83785b42d61f906e58d0c909f7b30bd753b2347
yyamnk <y.yamnk@gmail.com>
2017-04-04 18:58:14 +0900

    upd: *rc, proxy settings

--- bin/proxy_on.sh
+++ bin/proxy_on.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 #****************** proxy_on.sh *******************
 # created: 2014/11/07
-# Last Change:2015-Feb-09.
+# Last Change:2017-Apr-04.
 #
 # --- abst ---
 # nut proxyをonにする.
 #************************************************************
-export http_proxy=http://proxy.nagaokaut.ac.jp:8080/
+# export http_proxy=http://proxy.nagaokaut.ac.jp:8080/
+export http_proxy=http://proxy.cc.utsunomiya-u.ac.jp:8080/
 export HTTP_PROXY=$http_proxy
 export https_proxy=$http_proxy
 export HTTPS_PROXY=$http_proxy
 export ftp_proxy=$http_proxy
 export FTP_PROXY=$http_proxy
-export no_proxy=.local,169.254/16,10.10.10.10,127.0.0.1
+export no_proxy=.local,169.254/16,127.0.0.1
 export use_proxy=yes
 
-echo "NUT proxy enable"
+echo "proxy enable"
 
 ln -sf $HOME/.dotfiles/.wgetrc $HOME/.wgetrc
 echo "linked wgetrc"
