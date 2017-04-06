04d6a5ed64ee05afa82e593a79c4d2fbed7e18c0
yyamnk <y.yamnk@gmail.com>
2017-04-06 09:41:44 +0900

    upd: proxy_on, off

--- bin/proxy_off.sh
+++ bin/proxy_off.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #****************** proxy_off.sh *******************
 # created: 2014/11/08
-# Last Change:2015-Feb-09.
-#
-# --- abst ---
-# nut proxy off
+# Last Change:2017-Apr-06.
 #************************************************************
+echo "proxy disabling"
+
 unset http_proxy
 unset HTTP_PROXY
 unset https_proxy
@@ -15,8 +14,20 @@ unset FTP_PROXY
 unset no_proxy
 export use_proxy=no
 
-echo "NUT proxy disable"
-
 # check file exist -e
 [ -e ~/.wgetrc ] && unlink ~/.wgetrc && echo "wgetrc is deleted"
 [ -e ~/.curlrc ] && unlink ~/.curlrc && echo "curlrc is deleted"
+
+# git
+# git config --global --unset http.proxy  # it keeps entry
+git config --global --remove-section http
+# git config --global --unset https.proxy  # it keeps entry
+git config --global --remove-section https
+
+# ssh
+[ -e ~/.ssh/config ] && unlink ~/.ssh/config && echo "ssh/config is deleted"
+
+# apt
+[ -e /etc/apt/apt.conf ] && \
+    sudo unlink /etc/apt/apt.conf && \
+    echo 'apt.conf is deleted'
--- bin/proxy_on.sh
+++ bin/proxy_on.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #****************** proxy_on.sh *******************
 # created: 2014/11/07
-# Last Change:2017-Apr-04.
-#
-# --- abst ---
-# nut proxyをonにする.
+# Last Change:2017-Apr-06.
 #************************************************************
-# export http_proxy=http://proxy.nagaokaut.ac.jp:8080/
-export http_proxy=http://proxy.cc.utsunomiya-u.ac.jp:8080/
+echo "proxy enabling"
+
+# PROXY=http://proxy.nagaokaut.ac.jp:8080
+PROXY=http://proxy.cc.utsunomiya-u.ac.jp:8080
+export http_proxy=$PROXY
 export HTTP_PROXY=$http_proxy
 export https_proxy=$http_proxy
 export HTTPS_PROXY=$http_proxy
@@ -16,10 +16,21 @@ export FTP_PROXY=$http_proxy
 export no_proxy=.local,169.254/16,127.0.0.1
 export use_proxy=yes
 
-echo "proxy enable"
-
 ln -sf $HOME/.dotfiles/.wgetrc $HOME/.wgetrc
 echo "linked wgetrc"
 
 ln -sf $HOME/.dotfiles/.curlrc $HOME/.curlrc
 echo "linked curlrc"
+
+# git
+git config --global http.proxy $PROXY
+git config --global https.proxy $PROXY
+
+# ssh
+ln -sf  ~/.dotfiles/ssh/config ~/.ssh/
+
+# apt
+if [ -e /etc/apt ]; then
+    echo 'linking apt.conf'
+    sudo ln -sf ~/.dotfiles/apt.conf /etc/apt/
+fi
