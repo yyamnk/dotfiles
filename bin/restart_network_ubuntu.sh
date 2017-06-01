55b1433e60417f8927a7008240504ac2ba31a778
yyamnk <y.yamnk@gmail.com>
2017-06-01 15:55:39 +0900

    upd: restart_nework_ubuntu

--- bin/restart_network_ubuntu.sh
+++ bin/restart_network_ubuntu.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #****************** .dotfiles/bin/restart_network_ubuntu.sh *******************
 # Created    : 2017-Apr-30
-# Last Change: 2017-Apr-30.
+# Last Change: 2017-May-08.
 #************************************************************
 
 # sometimes, bluetooth tethering does not work after sleep.
 # https://askubuntu.com/questions/761180/wifi-doesnt-work-after-suspend-after-16-04-upgrade
 
-sudo systemctl restart network-manager.service
+# sudo systemctl restart network-manager.service
+sudo /etc/init.d/networking restart
+
+sudo /etc/init.d/bluetooth restart
