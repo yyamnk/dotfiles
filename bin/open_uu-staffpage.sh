7be59d8bd5bf70ab0c1c1234ff5e69cc85be54e5
yyamnk <y.yamnk@gmail.com>
2017-07-11 19:31:51 +0900

    add: open_uu-staffpage.sh

---
+++ bin/open_uu-staffpage.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#****************** /home/yyamnk/.dotfiles/bin/open_uu-staffpage.sh *******************
+# Created    : 2017-Jul-10
+# Last Change: 2017-Jul-10.
+#************************************************************
+firefox -new-window 'https://johowww.jm.utsunomiya-u.ac.jp/staff/'
