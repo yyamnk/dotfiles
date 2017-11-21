6b184b8fcc44386fc99229900d81e9c3575b27b1
yyamnk <y.yamnk@gmail.com>
2017-11-21 11:03:51 +0900

    upd: ipython profile

--- .ipython/profile_yyamnk/startup/00-init.py
+++ .ipython/profile_yyamnk/startup/00-init.py
@@ -2,15 +2,16 @@
 # -*- coding: utf-8 -*-
 # ****************** 00-init.py *******************
 # Created	 : 2015-Sep-02
-# Last Change: 2016-Apr-19.
+# Last Change: 2017-Nov-21.
 # -----------------------------------
 # ipython 起動時に自動実行
 # ************************************************************
 
 from imp import reload
 import numpy as np
-import matplotlib
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from pandas import Series, DataFrame
 import pandas as pd
@@ -18,11 +19,17 @@ from pprint import pprint as pp
 import sys
 import pickle
 import math
-# figure settings
-import matplotlib as mpl
-import seaborn as sns
+
+# default figure settings
 sns.set_context('poster',
-                font_scale=1.2,
-                rc={'figure.figsize': [8.0, 6.0], "lines.linewidth": 1.75})
+                font_scale=1.3,
+                rc={'figure.figsize': [8.0, 6.0],
+                    "lines.linewidth": 2.0})
+plt.style.use('seaborn-ticks')
+# デフォルトsans-serifはubuntuにない
+mpl.rcParams['font.family'] = ["DejaVu Sans"]
+mpl.rcParams['mathtext.fontset'] = 'stix' # デフォルトsansはubuntuにない．
+# legend style
 mpl.rcParams['legend.frameon'] = True
 mpl.rcParams['legend.shadow'] = True
+mpl.use('Qt5Agg')  # savefigと表示を一致させる．ipythonのバックエンドに合わせる
