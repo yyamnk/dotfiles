f97bed854e48ac8e92f3a1b0600fb3376a3a1ef0
yyamnk <y.yamnk@gmail.com>
2017-11-21 11:44:11 +0900

    upd: open_calendar

--- bin/open_calendar.sh
+++ bin/open_calendar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #****************** /home/yyamnk/.dotfiles/bin/open_calendar.sh *******************
 # Created    : 2017-Apr-10
-# Last Change: 2017-Jun-13.
+# Last Change: 2017-Nov-13.
 #************************************************************
-firefox -new-window 'https://calendar.google.com/calendar/render#main_7|month'
+google-chrome --app https://calendar.google.com/calendar/render#main_7|month
