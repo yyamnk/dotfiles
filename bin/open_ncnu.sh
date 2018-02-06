57d18b69aa20f7f3634295ab921144896580adb7
yyamnk <y.yamnk@gmail.com>
2018-02-06 09:50:44 +0900

    fix: bin/open_ncnu.sh, not app mode to use 1password

--- bin/open_ncnu.sh
+++ bin/open_ncnu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #****************** open_ncnu.sh *******************
 # Created    : 2017-Jul-26
-# Last Change: 2018-Feb-02.
+# Last Change: 2018-Feb-05.
 #************************************************************
 # firefox -new-window 'https://zaikaiweb.jm.utsunomiya-u.ac.jp/ncnu/login/login.jsp'
-google-chrome --app="https://zaikaiweb.jm.utsunomiya-u.ac.jp/ncnu/login/login.jsp"
+# google-chrome --app="https://zaikaiweb.jm.utsunomiya-u.ac.jp/ncnu/login/login.jsp"
+google-chrome https://zaikaiweb.jm.utsunomiya-u.ac.jp/ncnu/login/login.jsp
