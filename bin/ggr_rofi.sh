4048a88e8e9dfc74432c0a276aaab32292894a7b
yyamnk <y.yamnk@gmail.com>
2018-04-27 09:15:57 +0900

    add: bin/ggr_rofi, open_contacts

---
+++ bin/ggr_rofi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#****************** ggr_rofi.sh *******************
+# Created    : 2018-Feb-06
+# Last Change: 2018-Feb-06.
+#------------------------------------------------------------
+# ggr via rofi
+# simplified https://github.com/miroslavvidovic/rofi-scripts
+# usage:
+#   ggr_rofi.sh
+#************************************************************
+
+main() {
+  query=$( (echo ) | rofi  -dmenu -fuzzy -location 0 -p "Query > " )
+  if [[ -n "$query" ]]; then
+    url="https://www.google.com/search?q="$query
+    xdg-open "$url"
+  else
+    rofi -show -e "No query provided."
+  fi
+}
+
+main
+
+exit 0
--- bin/open_contacts.sh
+++ bin/open_contacts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #****************** open_contacts.sh *******************
 # Created    : 2017-Jun-05
-# Last Change: 2017-Jun-13.
+# Last Change: 2018-Apr-23.
 #************************************************************
-firefox -new-window 'https://contacts.google.com/'
+# firefox -new-window 'https://contacts.google.com/'
+google-chrome --app https://contacts.google.com/
