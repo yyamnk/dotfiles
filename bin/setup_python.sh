08f97d5ffeea6574f4d1b2d278ded2f541196a95
yyamnk <y.yamnk@gmail.com>
2018-08-24 23:29:13 +0900

    upd: python setup

--- bin/setup_python.sh
+++ bin/setup_python.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 #****************** bin/setup_python.sh *******************
 # created: 2015-Feb-12
-# Last Change: 2018-Jul-23.
+# Last Change: 2018- 8月-23.
 #************************************************************
 
 # get anaconda
@@ -17,6 +17,7 @@ fi
 
 # get packages
 echo 'start install conda packages'
+conda config --add channels conda-forge
 conda install \
     numpy \
     scipy \
@@ -31,35 +32,33 @@ conda install \
     seaborn \
     dill \
     Pillow \
-    redis
+    redis \
+    pep8 \
+    alembic \
+    arrow \
+    rq \
+    GitPython
+
+
 echo ''
 echo 'start install pip packages'
-pip install alembic
-pip install restview # rst viewer. depending 'readme'
+# pip install restview # rst viewer. depending 'readme'
 # check OSX bug for installing readme-0.6.0, http://bugs.python.org/issue24633
 # to fix it,
 # cd ~/.pyenv/versions/miniconda3-3.9.1/lib/python3.4/site-packages
 # mv README README.txt
-pip install pudb
-pip install arrow # better datetime
-pip install rq    # simple job-queue
+# pip install pudb
 # pip install git+https://github.com/Supervisor/supervisor.git # 4.0でpython3対応
 pip install git+ssh://git@github.com/Supervisor/supervisor.git # via ssh
-pip install peewee # simplo ORM
-pip install GitPython
-pip install click
+pip install peewee # simplo ORM, conda-forgeだと他のパッケージがダウングレードされる
 pip install rq-dashboard
 pip install prettyplotlib
-
-# syntax
-conda install pep8
-pip install git+ssh://git@github.com/yyamnk/pyflakes.git
-
-# scheduler -> use supervisor
-# pip install APScheduler
-# pip install python-daemon-3K
 
 # evolutionary computation framework
 pip install deap
 
 # pip install git+https://github.com/sdispater/orator.git
+
+# scheduler -> use supervisor
+# pip install APScheduler
+# pip install python-daemon-3K
