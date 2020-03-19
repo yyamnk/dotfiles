2a3acfc437de9220c190bab9b15a958844f1edee
yyamnk <y.yamnk@gmail.com>
2020-03-19 15:39:01 +0900

    upd: bin/*.sh for univ.

--- bin/open_ncnu.sh
+++ bin/open_ncnu.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #****************** open_ncnu.sh *******************
 # Created    : 2017-Jul-26
-# Last Change: 2018-Feb-05.
+# Last Change: 2020-Jan-27.
 #************************************************************
 # firefox -new-window 'https://zaikaiweb.jm.utsunomiya-u.ac.jp/ncnu/login/login.jsp'
 # google-chrome --app="https://zaikaiweb.jm.utsunomiya-u.ac.jp/ncnu/login/login.jsp"
-google-chrome https://zaikaiweb.jm.utsunomiya-u.ac.jp/ncnu/login/login.jsp
+# google-chrome https://zaikaiweb.jm.utsunomiya-u.ac.jp/ncnu/login/login.jsp
+# v-3
+google-chrome https://zaikaiweb.jm.utsunomiya-u.ac.jp/zkweb
---
+++ bin/open_udai_mail.sh
@@ -0,0 +1,1 @@
+google-chrome --app https://uumail.cc.utsunomiya-u.ac.jp/am_bin/ammain/top?id=8406_8471577
