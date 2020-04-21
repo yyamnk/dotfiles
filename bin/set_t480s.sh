8c9947a93d33c0fe40b1235fb9dd9b409b35d06a
yyamnk <y.yamnk@gmail.com>
2020-04-21 20:42:49 +0900

    upd: set_t480s

--- bin/set_t480s.sh
+++ bin/set_t480s.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #****************** .dotfiles/bin/set_t460s.sh *******************
 # Created    : 2017-Apr-06
-# Last Change: 2018- 8月-22.
+# Last Change: 2020-Apr-21.
 #************************************************************
 sleep 3
 
 # common xkb settings
-$HOME/.dotfiles/bin/set_myxkb.sh
+# $HOME/.dotfiles/bin/set_myxkb.sh
 
 # replace Print to Super_R
 # keycode 107 = Print Sys_Req Print Sys_Req
 xmodmap -e 'keycode 107 = Super_R'  # #107 is print-screen key
+
+setxkbmap -option ctrl:swapcaps
